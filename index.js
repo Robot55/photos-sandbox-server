6f3ebea59db102e4f8268248c9449d34d444f1ca
Robot55 <robot55@gmail.com>
2019-04-09 01:11:16 +0300

    fixed database. seems to work now. revamp client before pushing to prod

--- index.js
+++ index.js
@@ -1,37 +1,50 @@
-var sha1 = require('sha1');
-var express    = require('express');
+var sha1 = require('sha1')
+var express    = require('express')
+var cors = require('cors')
+var cookieParser = require('cookie-parser')
 require('dotenv').config()
-var app        = express();
-var port = process.env.PORT || 3000;        // set our port
-var passport = require('passport');
-var request = require ('request');
-var GoogleStrategy = require('passport-google-oauth20').Strategy;
-var myToken = '';
-var mongoose = require('mongoose');
+var app        = express()
+app.use(cookieParser())
+app.use(cors())
+var port = process.env.PORT || 3000       // set our port
+var passport = require('passport')
+var request = require ('request')
+var GoogleStrategy = require('passport-google-oauth20').Strategy
+var myToken = ''
+var mongoose = require('mongoose')
+var findOrCreate = require('mongoose-findorcreate')
 
 
 
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true})
 var Schema = mongoose.Schema;
 
 var userSchema = new mongoose.Schema({
   name: String,
   token: String,
 });
+userSchema.plugin(findOrCreate)
 
-
-var User = mongoose.model('User', userSchema);
+var User = mongoose.model('User', userSchema)
 
 var pairingSchema = new mongoose.Schema({
   hash: String,
   user: {type: Schema.Types.ObjectId, ref: 'User'}
 });
 
-var Pairing = mongoose.model('Pairing', pairingSchema);
+var Pairing = mongoose.model('Pairing', pairingSchema)
 
 // POST https://photoslibrary.googleapis.com/v1/mediaItems:search
 
 app.get('/photos', function(req, res) {
+	if (!req.query.hash){
+		
+		res.json({
+			"error": "please authenticate",
+			"errorCode": 401	
+
+		})
+	}
 	request(
 		{
 			url: 'https://photoslibrary.googleapis.com/v1/mediaItems:search',
@@ -57,26 +70,36 @@ app.get('/photos', function(req, res) {
 			headers:{'Authorization': 'Bearer ' +myToken}
 		
 		}, function(err, response, body){
-			res.json(body.mediaItems);
-			console.log(body);  
+			if (err) {
+				res.json({
+				"error": "please authenticate",
+				"errorCode": 401	
+				})
+				return
+			}
+			res.json(body.mediaItems)
+			console.log(body)  
 	  })
     
-});
+})
 
 app.get('/login', function(req, res) {
-		res.send('GSuite Login Completed.\n Unity and AFrame Apps can now use APIs');
+		res.send('GSuite Login Completed.\n Unity and AFrame Apps can now use APIs')
 	
-});
-app.use(express.static('public'));
+})
+app.use(express.static('public'))
 
 app.get('/createNewPairing', function(req, res) {
 		
-		var newPairing = new Pairing({ hash: sha1((new Date()-0)+process.env.HASH_SALT) });
+		var newPairing = new Pairing({ 'hash': sha1((new Date()-0)+process.env.HASH_SALT) })
 		newPairing.save(function (err, newPairing) {
-			if (err) return console.error(err);
-			pairingUrl = process.env.MY_DOMAIN+'/auth/google?hash='+newPairing.hash;
-			res.send(pairingUrl);
-		 });
+			if (err) return console.error(err)
+			pairingUrl = process.env.MY_DOMAIN+'/pair/'+newPairing.hash
+			res.json{
+				"url": pairingUrl,
+				"hash": newPairing.hash
+			}
+		 })
 		
 		
 		
@@ -96,30 +119,34 @@ passport.use(new GoogleStrategy({
 	passReqToCallback: true
   },
   function(req, token, tokenSecret, profile, done) {
-      User.findOrCreate({ name: profile.id }, function (err, user) {
-        Pairing.find({hash: req._toParam}, function (err, pairing){
-			user.token=token;
-			pairing.user=user;
-			pairing.save();
-		});
-		return done(err, user);
-      });
+      User.findOrCreate({ 'name': profile.id }, function (err, user) {
+		console.log({'hash': req.cookies.hash})
+		Pairing.findOne({'hash': req.cookies.hash}, function (err, pairing){
+			if (err) return console.error(err)
+			user.token=token
+			user.save()
+			pairing.user=user
+			pairing.save()
+			
+			return done()
+		})
+		//return done(err, user)
+	  })
   }
-));
+))
 
 // GET /auth/google
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  The first step in Google authentication will involve redirecting
 //   the user to google.com.  After authorization, Google will redirect the user
 //   back to this application at /auth/google/callback
-app.get('/auth/google',function(req,res,next){
-			req._toParam = req.params.hash;
-			passport.authenticate('google', { scope: ['profile','https://www.googleapis.com/auth/photoslibrary.readonly']}, function (req, res, next){
-				return next();
-			});
-			}
-		);
+app.get('/auth/google',passport.authenticate('google', { scope: ['profile','https://www.googleapis.com/auth/photoslibrary.readonly']}));
 
+app.get('/pair/:hash', function(req,res){
+			res.cookie('hash', req.params.hash)
+			res.redirect('/auth/google')
+			}
+		)
 // GET /auth/google/callback
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  If authentication fails, the user will be redirected back to the
